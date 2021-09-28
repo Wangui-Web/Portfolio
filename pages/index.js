3ab8b195cadca335ffe4bcb4ddfb652317dca37a
Wangui <wanguiwamutitu@gmail.com>
2021-09-28 21:49:54 +0300

    Changes in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head"
 export default function Home() {
   return (
     <Head>
-      <title>Wangui's Portfolio</title>
+      <title>Portfolio</title>
       <meta name="keywords" content="full stack developer portfolio" />
     </Head>
   )
